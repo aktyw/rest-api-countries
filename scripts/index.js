d90b2dbcd69b6a7b25abdc538d45a187dd70af2d
J0gaB0nito <arek.tywonek@gmail.com>
2022-11-20 05:12:29 +0100

    add country components

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,13 @@
-console.log('test log');
-
-async function getRequest() {
+async function getCountryData() {
   try {
+    const url = 'https://restcountries.com/v3.1/all';
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+    return data;
   } catch (error) {
     console.log(error);
   }
 }
+
+getCountryData();
